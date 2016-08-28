2baf20ec5e62c909388dbd609f1bf323066ff052
Margaret <eutopia@gmail.com>
2016-08-28 14:19:02 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,37 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  class << self
+  def first_five
+	limit(5)
+  end
+
+  def dinghy
+  	where("length<=20")
+  end
+
+  def ship
+  	where("length>=20")
+  end
+
+  def last_three_alphabetically
+  	order("name desc").limit(3)
+  end
+
+  def without_a_captain
+  	where("captain_id is null")
+  end
+
+  def sailboats
+  	joins(:classifications).where("classifications.name = 'Sailboat'")
+  end
+
+  def with_three_classifications
+  	joins(:classifications).group("boats.name").having("count() = 3")
+  end
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  	joins(boats: :classifications).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+  	joins(boats: :classifications).where("classifications.name = 'Sailboat'").distinct
+  end
+
+  def self.motorboats
+  	joins(boats: :classifications).where("classifications.name = 'Motorboat'").distinct
+  end
+  
+  def self.talented_seamen
+  	where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id)).distinct
+  end
+
+  def self.non_sailors
+  	where("id NOT IN (?)", self.sailors.pluck(:id)).distinct
+  end
 end
